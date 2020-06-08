07a901de29324b5d4c69fe1e584554c5c466de79
l-c-w <vienn1985@gmail.com>
2020-06-08 17:41:55 +0900

    cart

--- shop_jardin/src/main/java/com/javalec/ex/Controller/PayController.java
+++ shop_jardin/src/main/java/com/javalec/ex/Controller/PayController.java
@@ -1,13 +1,30 @@
 package com.javalec.ex.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.javalec.ex.Dao.PayDao;
 
 @Controller
 public class PayController {
 
 	@Autowired
 	private SqlSession sqlsession;
+
+	// 카트 뷰 보기
+	@RequestMapping("mypage/cart")
+	public String cart_view(HttpServletRequest request, Model model) {
+
+		String id = "qwer";
+		PayDao payDao = sqlsession.getMapper(PayDao.class);
+		model.addAttribute("list", payDao.cart_view(id));
+
+		return "mypage/cart";
+	}
 
 }
--- shop_jardin/src/main/java/com/javalec/ex/Dao/PayDao.java
+++ shop_jardin/src/main/java/com/javalec/ex/Dao/PayDao.java
@@ -1,5 +1,12 @@
 package com.javalec.ex.Dao;
 
+import java.util.ArrayList;
+
+import com.javalec.ex.Dto.PayDto.CartDto;
+
 public interface PayDao {
+
+	// 장바구니 보기
+	public ArrayList<CartDto> cart_view(String id);
 
 }

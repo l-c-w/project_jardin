b80dd86cdc9e334e9f6757b6dc5ec2c9728428be
hj <61442652+superRookIt@users.noreply.github.com>
2020-06-08 16:41:52 +0900

    관리자페이지 고객리스트 출력만

--- shop_jardin/src/main/java/com/javalec/ex/Controller/AController.java
+++ shop_jardin/src/main/java/com/javalec/ex/Controller/AController.java
@@ -1,10 +1,15 @@
 package com.javalec.ex.Controller;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.javalec.ex.Dao.MDao;
+import com.javalec.ex.Dto.MDto.Member_Dto;
 
 @Controller
 public class AController {
@@ -21,8 +26,18 @@ public class AController {
 	
 	
 	
+	
 	@RequestMapping("admin/admin_mlist")
 	public String admin_mlist(Model model) {
+		
+		MDao dao = sqlsession.getMapper(MDao.class);
+		
+		ArrayList<Member_Dto> mdtos = dao.m_list();
+		
+		model.addAttribute("m_count", dao.m_count());
+		
+		model.addAttribute("m_list", mdtos);
+		
 		return "admin/admin_mlist";
 	}
 	
